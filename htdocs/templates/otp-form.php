5711f5bbd266d41720cc7ea600111e34b56e2440
mahalakshmi <mahalakshmi4072003.com>
2024-06-15 16:58:55 +0000

    otp

---
+++ htdocs/templates/otp-form.php
@@ -0,0 +1,15 @@
+<main class="px-3">
+<div class="login">
+<div class="form-wrapper">
+        <h2>OTP Verfication </h2>
+        <form action="#">
+            <div class="form-control">
+                <input type="text" required>
+                <label>Email your OTP</label>
+            </div>
+            <button type="submit">SEND</button>
+            <p id="signup">Send OTP<a href="signup.php">Resent</a></p> 
+        </form>
+    </div>
+</div>
+</main>
